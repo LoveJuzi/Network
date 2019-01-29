5ec2de93e46dc976c3c76e27b74f97690545f56b
LoveJuzi <1071182804@qq.com>
2019-01-29 14:12:27 +0800

    daytimetcpcli

---
+++ daytimetcpcli/build/CMakeFiles/daytimetcpcli.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/daytimetcpcli.dir/main.cpp.o"
+  "daytimetcpcli.pdb"
+  "daytimetcpcli"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/daytimetcpcli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
