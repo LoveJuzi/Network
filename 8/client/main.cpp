49f305cf69b80c2ba107fd31cfaff9c19732ceae
LoveJuzi <1071182804@qq.com>
2019-02-01 09:21:19 +0800

    update 8

---
+++ 8/client/main.cpp
@@ -0,0 +1,258 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <algorithm>
+
+#define MAXLINE 1024
+#define SA struct sockaddr
+#define SERV_PORT 9877
+#define LISTENQ 5
+
+int Socket(int __domain, int __type, int __protocol)
+{
+   int fd;
+   fd = socket(__domain, __type, __protocol);
+   if (fd <= 0)
+   {
+      exit(-1);
+   }
+
+   return fd;
+}
+
+int Bind(int __fd, __CONST_SOCKADDR_ARG __addr, socklen_t __len)
+{
+   if (bind(__fd, __addr, __len) < 0)
+   {
+      exit(-1);
+   }
+
+   return 0;
+}
+
+int Listen(int __fd, int __n)
+{
+   if (listen(__fd, __n) < 0)
+   {
+      exit(-1);
+   }
+
+   return 0;
+}
+
+int Accept(int __fd, __SOCKADDR_ARG __addr, socklen_t *__restrict __addr_len)
+{
+   int fd;
+
+   fd = accept(__fd, __addr, __addr_len);
+
+   if (fd <= 0)
+   {
+      exit(-1);
+   }
+
+   return fd;
+}
+
+int Close(int __fd)
+{
+   if (close(__fd) < 0)
+   {
+      exit(-1);
+   }
+
+   return 0;
+}
+
+int Connect(int __fd, __CONST_SOCKADDR_ARG __addr, socklen_t __len)
+{
+   if (connect(__fd, __addr, __len) < 0)
+   {
+      exit(-1);
+   }
+
+   return 0;
+}
+
+ssize_t Read(int __fd, void *__buf, size_t __nbytes)
+{
+   auto n = read(__fd, __buf, __nbytes);
+   if (n < 0)
+   {
+      exit(-1);
+   }
+
+   return n;
+}
+
+ssize_t readn(int fd, void *vptr, size_t n)
+{
+   size_t nleft;
+   ssize_t nread;
+   char *ptr;
+
+   ptr = (char *)vptr;
+   nleft = n;
+
+   while (nleft > 0)
+   {
+      if ((nread = read(fd, ptr, nleft)) < 0)
+      {
+         if (errno == EINTR)
+         {
+            nread = 0;
+         }
+         else
+         {
+            return -1;
+         }
+      }
+      else if (nread == 0)
+      {
+         break;
+      }
+
+      puts("readn");
+
+      nleft -= nread;
+      ptr += nread;
+   }
+
+   return (n - nleft);
+}
+
+ssize_t Readn(int fd, void *vptr, size_t n)
+{
+   if (readn(fd, vptr, n) < 0)
+   {
+      exit(-1);
+   }
+
+   return n;
+}
+
+ssize_t writen(int fd, const void *vptr, size_t n)
+{
+   size_t nleft;
+   ssize_t nwritten;
+
+   const char *ptr;
+
+   ptr = (const char *)vptr;
+   nleft = n;
+   while (nleft > 0)
+   {
+      if ((nwritten = write(fd, ptr, nleft)) <= 0)
+      {
+         if (nwritten < 0 && errno == EINTR)
+         {
+            nwritten = 0;
+         }
+         else
+         {
+            return -1;
+         }
+      }
+      nleft -= nwritten;
+      ptr += nwritten;
+   }
+
+   return n;
+}
+
+ssize_t Writen(int fd, const void *vptr, size_t n)
+{
+   if (writen(fd, vptr, n) < 0)
+   {
+      printf("writen error\n");
+      exit(-1);
+   }
+
+   return n;
+}
+
+void str_cli(int sockfd)
+{
+   char sendline[MAXLINE];
+   char recvline[MAXLINE];
+
+   int stdineof = 0;
+   for (;;)
+   {
+      int maxfdp1;
+      fd_set rset;
+
+      FD_ZERO(&rset);
+      if (stdineof == 0)
+      {
+         FD_SET(fileno(stdin), &rset);
+      }
+      FD_SET(sockfd, &rset);
+      maxfdp1 = std::max(fileno(stdin), sockfd) + 1;
+
+      select(maxfdp1, &rset, NULL, NULL, NULL);
+
+      if (FD_ISSET(fileno(stdin), &rset))
+      {
+         bzero(sendline, sizeof(sendline));
+         auto n = Read(fileno(stdin), sendline, MAXLINE - 1);
+         if (n == 0)
+         {
+            stdineof = 1;
+            shutdown(sockfd, SHUT_WR);
+            FD_CLR(fileno(stdin), &rset);
+         }
+         else
+         {
+            Writen(sockfd, sendline, strlen(sendline));
+         }
+      }
+
+      if (FD_ISSET(sockfd, &rset))
+      {
+         auto n = Read(sockfd, recvline, MAXLINE - 1);
+
+         if (n == 0)
+         {
+            if (stdineof == 1)
+            {
+               return;
+            }
+            else
+            {
+               puts("str_cli: server terminated prematurely");
+               exit(-1);
+            }
+         }
+
+         recvline[n] = '\0';
+         fputs(recvline, stdout);
+      }
+   }
+}
+
+int main(int argc, char **argv)
+{
+   int sockfd;
+   struct sockaddr_in servaddr;
+
+   sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+   bzero(&servaddr, sizeof(servaddr));
+   servaddr.sin_family = AF_INET;
+   servaddr.sin_port = htons(SERV_PORT);
+   inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+
+   Connect(sockfd, (SA *)&servaddr, sizeof(servaddr));
+
+   str_cli(sockfd);
+
+   return 0;
+}
---
+++ 8/server/main.cpp
@@ -0,0 +1,331 @@
+#include <iostream>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <syslog.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/select.h>
+#include <sys/time.h>
+
+#include <set>
+#include <vector>
+#include <algorithm>
+
+#define MAXLINE 1024
+#define SA struct sockaddr
+#define SERV_PORT 9877
+#define LISTENQ 5
+#define MAXFD 64
+
+int Socket(int __domain, int __type, int __protocol)
+{
+    int fd;
+    fd = socket(__domain, __type, __protocol);
+    if (fd <= 0)
+    {
+        exit(-1);
+    }
+
+    return fd;
+}
+
+int Bind(int __fd, __CONST_SOCKADDR_ARG __addr, socklen_t __len)
+{
+    if (bind(__fd, __addr, __len) < 0)
+    {
+        exit(-1);
+    }
+
+    return 0;
+}
+
+int Listen(int __fd, int __n)
+{
+    if (listen(__fd, __n) < 0)
+    {
+        exit(-1);
+    }
+
+    return 0;
+}
+
+int Accept(int __fd, __SOCKADDR_ARG __addr, socklen_t *__restrict __addr_len)
+{
+    int fd;
+
+    fd = accept(__fd, __addr, __addr_len);
+
+    if (fd <= 0)
+    {
+        exit(-1);
+    }
+
+    return fd;
+}
+
+int Close(int __fd)
+{
+    if (close(__fd) < 0)
+    {
+        exit(-1);
+    }
+
+    return 0;
+}
+
+int Connect(int __fd, __CONST_SOCKADDR_ARG __addr, socklen_t __len)
+{
+    if (connect(__fd, __addr, __len) < 0)
+    {
+        exit(-1);
+    }
+
+    return 0;
+}
+
+ssize_t readn(int fd, void *vptr, size_t n)
+{
+    size_t nleft;
+    ssize_t nread;
+    char *ptr;
+
+    ptr = (char *)vptr;
+    nleft = n;
+
+    while (nleft > 0)
+    {
+        if ((nread = read(fd, ptr, nleft)) < 0)
+        {
+            if (errno == EINTR)
+            {
+                nread = 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        else if (nread == 0)
+        {
+            break;
+        }
+
+        nleft -= nread;
+        ptr += nread;
+    }
+
+    return (n - nleft);
+}
+
+ssize_t Readn(int fd, void *vptr, size_t n)
+{
+    if (readn(fd, vptr, n) < 0)
+    {
+        exit(-1);
+    }
+
+    return n;
+}
+
+ssize_t writen(int fd, const void *vptr, size_t n)
+{
+    size_t nleft;
+    ssize_t nwritten;
+
+    const char *ptr;
+
+    ptr = (const char *)vptr;
+    nleft = n;
+    while (nleft > 0)
+    {
+        if ((nwritten = write(fd, ptr, nleft)) <= 0)
+        {
+            if (nwritten < 0 && errno == EINTR)
+            {
+                nwritten = 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        nleft -= nwritten;
+        ptr += nwritten;
+    }
+
+    return n;
+}
+
+ssize_t Writen(int fd, const void *vptr, size_t n)
+{
+    if (writen(fd, vptr, n) < 0)
+    {
+        printf("writen error\n");
+        exit(-1);
+    }
+
+    return n;
+}
+
+void str_echo(int sockfd)
+{
+    ssize_t n;
+    char buf[MAXLINE];
+
+again:
+    while ((n = read(sockfd, buf, MAXLINE)) > 0)
+    {
+        Writen(sockfd, buf, n);
+    }
+
+    if (n < 0 && errno == EINTR)
+    {
+        goto again;
+    }
+    else if (n < 0)
+    {
+        exit(-1);
+    }
+}
+
+void sig_chld(int signo)
+{
+    pid_t pid;
+    int stat;
+
+    // pid = wait(&stat);
+    while ((pid = waitpid(-1, &stat, WNOHANG)) > 0)
+        ;
+
+    return;
+}
+
+int daemon_init(const char *pname, int facility) {
+    int i;
+    pid_t pid;
+
+    if ( (pid=fork()) < 0) {
+        return -1;
+    } else if (pid) {
+        _exit(0);
+    }
+
+    /* child 1 continues... */
+
+    if (setsid() < 0) {            // become session leader
+        return (-1);
+    }
+
+    signal(SIGHUP, SIG_IGN);
+
+    if ( (pid=fork()) < 0) {
+        return -1;
+    } else if (pid ) {
+        _exit(0);
+    }
+
+    /* child 2 continues... */
+
+    chdir("/");
+
+    /* close off file descriptors */
+    for (i=0; i<MAXFD; i++) {
+        close(i);
+    }
+
+    /* redirect stdin, stdout, and stderr to /dev/null */
+    open("/dev/null", O_RDONLY);
+    open("/dev/null", O_WRONLY);
+    open("/dev/null", O_RDWR);
+
+    openlog(pname, LOG_PID, facility);
+}
+
+int main(int argc, char **argv)
+{
+    int listenfd, connfd;
+    pid_t childpid;
+    socklen_t clilen;
+    struct sockaddr_in cliaddr, servaddr;
+
+    daemon_init(argv[0], 0);
+
+    // 构造listen sockert
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+    // 初始化 servaddr 相关信息
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(SERV_PORT);
+
+    // 绑定服务到指定的内核
+    Bind(listenfd, (SA *)&servaddr, sizeof(servaddr));
+
+    // 监听服务
+    Listen(listenfd, LISTENQ);
+
+    // 所有的客户端申请的fd
+    std::set<int> fdset;
+
+    // 并发处理业务
+    for (;;)
+    {
+        fd_set allset;
+
+        FD_ZERO(&allset);
+
+        auto maxfdp1 = listenfd + 1;
+
+        FD_SET(listenfd, &allset);
+        for (auto itr = fdset.begin(); itr != fdset.end(); ++itr)
+        {
+            FD_SET(*itr, &allset);
+            maxfdp1 = std::max(maxfdp1, *itr + 1);
+        }
+
+        select(maxfdp1, &allset, NULL, NULL, NULL);
+
+        if (FD_ISSET(listenfd, &allset))
+        {
+            clilen = sizeof(cliaddr);
+            connfd = Accept(listenfd, (SA *)&cliaddr, &clilen);
+            fdset.insert(connfd);
+        }
+
+        std::vector<int> rmfd;
+        for (auto itr = fdset.begin(); itr != fdset.end(); ++itr)
+        {
+            if (FD_ISSET(*itr, &allset))
+            {
+                ssize_t n;
+                char buf[MAXLINE];
+
+                bzero(buf, sizeof(buf));
+                n = read(*itr, buf, MAXLINE);
+                if (n == 0)
+                {
+                    rmfd.push_back(*itr);
+                    Close(*itr);
+                }
+                else
+                {
+                    Writen(*itr, buf, n);
+                }
+            }
+        }
+
+        for (auto itr = rmfd.begin(); itr != rmfd.end(); ++itr)
+        {
+            fdset.erase(*itr);
+        }
+    }
+
+    exit(0);
+}
